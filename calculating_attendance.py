281302e26df49f22c1f05c232ad79f0d893c9700
Viraaj Dubey <Viraaj@Vikrams-MacBook-Pro.local>
2021-03-17 22:20:49 +0530

    Attendance & Weekday codes

---
+++ calculating_attendance.py
@@ -0,0 +1,11 @@
+# calculating_attendance
+
+total_class = 10
+class_attend = 8
+
+percentage_class_attended = (class_attend * 100) / total_class
+
+if percentage_class_attended < 75:
+    print('percentage of class attended is', percentage_class_attended, 'Hence, you are not allowed to sit in the exam')
+else:
+    print('percentage of class attended is', percentage_class_attended, 'Hence, you are allowed to sit in the exam')
---
+++ find_out_weekday.py
@@ -0,0 +1,22 @@
+# finding weekday with the given integer
+
+integer = 9
+
+week_day = 0
+
+if integer == 1:
+    print('Monday')
+elif integer == 2:
+    print('Tuesday')
+elif integer == 3:
+    print('Wednesday')
+elif integer == 4:
+    print('Thursday')
+elif integer == 5:
+    print('Friday')
+elif integer == 6:
+    print('Saturday')
+elif integer == 7:
+    print('Sunday')
+else:
+    print('Not valid')
